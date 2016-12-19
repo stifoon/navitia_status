51f9601614f6fd94e9fe477f504e701c4117b18b
stifoon <stephan.simart@gmail.com>
2016-12-19 19:27:19 +0100

    dd networks

--- get_navitia_status.py
+++ get_navitia_status.py
@@ -1,25 +1,51 @@
+# coding: utf8
+import ConfigParser
 import requests
 
-token='3b036afe-0110-4202-b9ed-99718476c2e0'
+config = ConfigParser.ConfigParser()
+config.readfp(open('get_navitia_status.ini'))
+my_token= config.get('DEFAULT','token')
 
-resp = requests.get('https://{}@api.navitia.io/v1/coverage/'.format(token))
+resp = requests.get('https://{}@api.navitia.io/v1/coverage/'.format(my_token))
+
+
 if resp.status_code != 200:
     # This means something went wrong.
     raise ApiError(format(resp.status_code))
 
 status_file = open('navitia_rt_status.csv', 'w')
+head_line= u'coverage;is_open_data;rt_count;proxy_count;networks\n'
+status_file.write(head_line.encode('utf-8'))
 
 total_network = 0
-status_file.write('coverage;rt_count;proxy_count\n')
 for region in resp.json()["regions"]:
+    #request for one coverage
     coverage_id = region['id']
-    url = 'https://{}@api.navitia.io/v1/coverage/'.format(token) + coverage_id + '/status'
-    coverage = requests.get(url)
-    realtime_contributors_count= len(coverage.json()["status"]["realtime_contributors"] or [])
+    print coverage_id
+    url = 'https://{}@api.navitia.io/v1/coverage/'.format(my_token) + coverage_id + '/status'
+    response = requests.get(url)
+
+    #reading the response 
+    is_open_data= response.json()['status']['is_open_data']
+
+    #any Traffic_report feeders ?
+    realtime_contributors_count= len(response.json()['status']['realtime_contributors'] or [])
+
+    #any proxy departures feeders ?
     proxies_count = 0
-    for realtime_proxies in coverage.json()["status"]["realtime_proxies"]:
+    for realtime_proxies in response.json()['status']['realtime_proxies']:
         proxies_count+= 1
-    line_to_print = '{cov_id};{rt_count};{prox_count}'.format(cov_id=coverage_id, rt_count=realtime_contributors_count, prox_count=proxies_count)
-    print line_to_print
-    status_file.write(line_to_print + '\n')
+
+    #networks
+    networks = u''
+    url = 'https://{}@api.navitia.io/v1/coverage/'.format(my_token) + coverage_id + '/networks'
+    response = requests.get(url)
+    for network in response.json()['networks']:
+        networks = networks + network['name'] + '\n'
+    networks = '"' + networks.encode('utf-8') + '"'
+
+    #write line
+    line_to_print = '{cov_id};{is_od};{rt_count};{prox_count};{netwk}'.format(cov_id=coverage_id, is_od=is_open_data, rt_count=realtime_contributors_count, prox_count=proxies_count, netwk=networks)
+    line_to_print = line_to_print + '\n'
+    status_file.write(line_to_print)
 
